be3475fca7523a4978bbd55d1662ae8f2bdeccce
Larry Gadallah <lgadallah@gmail.com>
2024-11-14 19:40:04 -0800

    MVP versions of build.sh and deploy.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,30 @@
 #! /bin/sh
+# set -e
+set -x
+
+echo -n "$(date): "
+
+# Change into the repo dir
+cd $(dirname "${0}")
+
+# Check if this is a git repo
+if ! [ -d .git ]; then
+  >&2 echo "$(pwd): Not a git repo, exiting..."
+  exit 1
+fi
+
+# Get the branch name
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+
+# Update the remote repo information
+git fetch --quiet origin
+
+# List remote and local commits
+REMOTE_COMMITS="$(git rev-list --left-right --count \
+  "origin/${BRANCH}...${BRANCH}" | awk '{ print $1 }')"
 
 # Check if remote has changed
-git status -sb | grep -q 'behind '
-if [ $? -eq 0 ] || [ ${1} = "-f" ]; then
+if [ ${REMOTE_COMMITS} -gt 0 ] || [ "${1}" = "-f" ]; then
   # If there are upstream changes, get them
   git pull --ff-only
 
@@ -14,5 +36,7 @@ if [ $? -eq 0 ] || [ ${1} = "-f" ]; then
   #bundle exec jekyll33 build --verbose --trace
   bundle exec jekyll33 build --verbose
 
-  doas /home/larry/techn0mad.github.io/deploy.sh
+  doas $(dirname ${0})/deploy.sh /var/www/htdocs/orcas.lan.gadallah.net/blog
+else
+  echo "No changes."
 fi
--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
 #! /bin/sh
 
-if [ -d /var/www/htdocs/orcas.lan.gadallah.net/blog ]; then
-  rm -fr /var/www/htdocs/orcas.lan.gadallah.net/blog
+if [ -d "${1}" ]; then
+  rm -fr "${1}"
 fi
 
-if [ -d /home/larry/techn0mad.github.io ]; then
-  cd /home/larry/techn0mad.github.io
-
-  if [ -d /home/larry/techn0mad.github.io/_site ]; then
-    cp -a _site /var/www/htdocs/orcas.lan.gadallah.net/blog
-  fi
+if [ -d ./_site ]; then
+  cp -a ./_site "${1}"
 fi
